60d3728faf1050009c63549cba1cff49022cdcca
Kamran <oops.legendary@gmail.com>
2022-06-29 14:22:21 +0430

    removed configure file

--- CoreApplication/src/main.cpp
+++ CoreApplication/src/main.cpp
@@ -1,13 +1,11 @@
 #include <QCoreApplication>
 #include <QDebug>
 
-#include "Qt6_config.h"
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    qInfo() << "Built using Qt v" QT_VERSION;
+    qInfo() << "Built using Qt v" QT_VERSION_STR;
     qInfo() << "Starting CoreApplication...";
 
     return a.exec();
--- WidgetsApplication/src/main.cpp
+++ WidgetsApplication/src/main.cpp
@@ -1,15 +1,13 @@
-
 #include <QApplication>
 #include <QDebug>
 
-#include "Qt6_config.h"
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    qInfo() << "Built using Qt v" QT_VERSION;
+    qInfo() << "Built using Qt v" QT_VERSION_STR;
     qInfo() << "Starting WidgetsApplication...";
 
     MainWindow w;
